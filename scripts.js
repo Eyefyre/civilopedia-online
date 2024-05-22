b5e1ba5e13b4c7e456c19e3aec3d970c624cbaa3
AM <amacklework@gmail.com>
2024-05-22 20:36:36 +0100

    Updated files

--- scripts.js
+++ scripts.js
@@ -56,8 +56,6 @@ var tag_mappings = {
 
 
 document.addEventListener("DOMContentLoaded", async function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     $("body").hide()
     current_language = (sessionStorage.getItem("locale") || "en");
     await fetch("./assets/data/translations_" + current_language + ".json")
@@ -78,11 +76,15 @@ document.addEventListener("DOMContentLoaded", async function () {
             content_mapping = data;
         })
         .catch(error => console.error(`Error loading ${language} content file:`, error));
-    await $("#language-dropdown").val(current_language)
-    await populate_dropdowns()
     current_category = data_mappings["categories"][0]
     current_section = current_category["sections"][0]
     current_item = current_section["items"][0]
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    for (var i = 0; i < tooltipTriggerList.length; i++) {
+        tooltip = tooltipTriggerList[i].getAttribute("data-bs-title")
+        tooltipTriggerList[i].setAttribute("data-bs-title", get_translation(current_language,tooltip));
+    }
     generate_view()
     create_listeners()
     set_heading()
@@ -91,80 +93,12 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 
-$(document).on('change', '#category-dropdown', function (e) {
-    current_category = data_mappings["categories"].find(item => item.id === this.value);
-    $("#section-dropdown").empty()
-    for (var i = 0; i < current_category["sections"].length; i++) {
-        if (i == 0) {
-            $("#section-dropdown").append("<option selected value='" + current_category["sections"][i]["id"] + "'>" + get_translation(current_language, current_category["sections"][i]["label"]) + "</option>");
-        }
-        else {
-            $("#section-dropdown").append("<option value='" + current_category["sections"][i]["id"] + "'>" + get_translation(current_language, current_category["sections"][i]["label"]) + "</option>");
-        }
-    }
-    $('#section-dropdown').trigger('change');
-});
-
-$(document).on('change', '#section-dropdown', function (e) {
-    current_section = current_category["sections"].find(item => item.id === this.value);
-    $("#item-dropdown").empty()
-    for (var i = 0; i < current_section["items"].length; i++) {
-        if (i == 0) {
-            $("#item-dropdown").append("<option selected value='" + current_section["items"][i]["id"] + "'>" + get_translation(current_language, current_section["items"][i]["label"]) + "</option>");
-        }
-        else {
-            $("#item-dropdown").append("<option value='" + current_section["items"][i]["id"] + "'>" + get_translation(current_language, current_section["items"][i]["label"]) + "</option>");
-        }
-    }
-    $('#item-dropdown').trigger('change');
-});
-
-$(document).on('change', '#item-dropdown', function (e) {
-    current_item = current_section["items"].find(item => item.id === this.value);
-    generate__mobile_view()
-});
-
-
-async function populate_dropdowns() {
-    $("#category-dropdown").empty()
-    for (var i = 0; i < data_mappings["categories"].length; i++) {
-        if (i == 15) {
-            $("#category-dropdown").append("<option selected value='" + data_mappings["categories"][i]["id"] + "'>" + get_translation(current_language, data_mappings["categories"][i]["label"]) + "</option>");
-        }
-        else {
-            $("#category-dropdown").append("<option value='" + data_mappings["categories"][i]["id"] + "'>" + get_translation(current_language, data_mappings["categories"][i]["label"]) + "</option>");
-        }
-    }
-    current_category = data_mappings["categories"].find(item => item.id === $("#category-dropdown").val());
-    $("#section-dropdown").empty()
-    for (var i = 0; i < current_category["sections"].length; i++) {
-        if (i == 1) {
-            $("#section-dropdown").append("<option selected value='" + current_category["sections"][i]["id"] + "'>" + get_translation(current_language, current_category["sections"][i]["label"]) + "</option>");
-        }
-        else {
-            $("#section-dropdown").append("<option value='" + current_category["sections"][i]["id"] + "'>" + get_translation(current_language, current_category["sections"][i]["label"]) + "</option>");
-        }
-    }
-    current_section = current_category["sections"].find(item => item.id === $("#section-dropdown").val());
-    $("#item-dropdown").empty()
-    for (var i = 0; i < current_section["items"].length; i++) {
-        if (i == 16) {
-            $("#item-dropdown").append("<option selected value='" + current_section["items"][i]["id"] + "'>" + get_translation(current_language, current_section["items"][i]["label"]) + "</option>");
-        }
-        else {
-            $("#item-dropdown").append("<option value='" + current_section["items"][i]["id"] + "'>" + get_translation(current_language, current_section["items"][i]["label"]) + "</option>");
-        }
-    }
-    current_item = current_section["items"].find(item => item.id === $("#item-dropdown").val());
-}
-
 function get_translation(language, key) {
-    //console.log("Getting Translation for " + key + " in " + language)
+    console.log(key)
     if (key in translations[language]) {
         return parse_tags(translations[language][key])
     }
     else {
-        //console.log(`\"${key}\" doesn't exist in ${language} file, parsing tags as plain`)
         return parse_tags(key)
     }
 
@@ -194,13 +128,11 @@ function create_listeners() {
 }
 
 Handlebars.registerHelper('translate', function (aString) {
-    //console.log("Handlebar Helper translate: " + aString)
     return get_translation(current_language, aString)
 })
 
 Handlebars.registerHelper('get_item_name', function (item_id) {
     item_name = get_info_from_item_id(item_id)["strings"]["title"]
-    //console.log("Handlebar Helper Item Name: " + item_name)
     return item_name
 })
 Handlebars.registerHelper('get_item_image', function (item_id) {
@@ -211,43 +143,35 @@ Handlebars.registerHelper('parse_tags', function (aString) {
     return parse_tags(aString)
 })
 
-Handlebars.registerHelper('log_info', function (aString) {
-    console.log(aString)
-})
 
 function generate_view() {
     $("#content-area").empty()
     var item_data = content_mapping.find(item => item.item_id === current_item.id);
-    //console.log("Curent item is " + JSON.stringify(item_data))
     var template = Handlebars.compile($("#" + item_data["view_id"]).html())
     $("#content-area").append(template(item_data))
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 }
 
-//Trigger on Category tab click
 $(document).on("click", ".category-tab", function () {
     current_category = data_mappings["categories"].find(item => item.id === this.value);
     current_section = current_category["sections"][0]
     current_item = current_section["items"][0]
     set_heading()
     generate_accordion_list()
-    
+
 });
 
 $(document).on("click", ".list-group-item", function () {
     current_section = current_category["sections"].find(item => item.id === $(this).parent().attr("value"));
     current_item = current_section["items"].find(item => item.id === $(this).attr("value"));
-    console.log("current item = " + current_item["id"])
     if (["item_1", "item_2", "item_3", "item_4", "item_5", "item_6", "item_7", "item_8", "item_9", "item_10", "item_11", "item_12", "item_13", "item_14", "item_15", "item_16"].includes(current_item["id"])) {
-        console.log("Checking home pages")
         check_home_pages()
-    }    
+    }
     generate_view()
 })
 
 function switch_category() {
-    console.log(current_category["id"])
     switch (current_category["id"]) {
         case "cat_1":
             document.querySelector('#home-tab').click();
@@ -308,11 +232,6 @@ function check_home_pages() {
     switch_category()
 }
 
-
-$(document).on("click", ".small-image", function () {
-    //console.log($(this).attr("value"))
-})
-
 function set_heading() {
     $("#current_heading").text(get_translation(current_language, current_category["label"]));
     $("#civilopedia-title").text(get_translation(current_language, "TXT_KEY_CIVILOPEDIA"));
@@ -349,6 +268,5 @@ function generate_accordion_list() {
 
 function get_info_from_item_id(item_id) {
     item_info = content_mapping.find(element => element.item_id === item_id);
-    //console.log("Current item is " + item_info)
     return item_info
 }
